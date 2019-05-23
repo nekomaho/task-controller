f0e87b3d12616916a1b1720b9776220ba462f2ac
nekomaho <nekosatoru@gmail.com>
2019-05-23 22:56:11 +0900

    Need required false

--- spec/models/task_relationship_spec.rb
+++ spec/models/task_relationship_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe TaskRelationship, type: :model do
   describe 'relation' do
-    it { is_expected.to belong_to(:previous).class_name('Task') }
-    it { is_expected.to belong_to(:next).class_name('Task') }
+    it { is_expected.to belong_to(:previous).required.class_name('Task') }
+    it { is_expected.to belong_to(:next).required.class_name('Task') }
   end
   describe '#previous' do
     it { is_expected.to validate_presence_of(:previous) }
